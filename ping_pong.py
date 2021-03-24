449a69145be49b96750bf14bb58c8b67fb276a31
John01Alves <johnalvessr@protonmail.com>
2021-03-24 15:39:50 -0300

    Contruindo a tela inicial.

--- ping_pong.py
+++ ping_pong.py
@@ -1,1 +1,19 @@
 import pygame
+
+# Cores.
+preto = 0, 0, 0
+branco = 255, 255, 255
+
+# Configurando a tela.
+tamanho = 800, 600
+tela = pygame.display.set_mode(tamanho)
+
+fim = False
+
+# Iniciando a tela.
+while not fim:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:  # Condição de saida.
+            fim = True
+    tela.fill(preto)
+    pygame.display.update()
