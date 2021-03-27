cfd5b4adf315ac13593012f80e47bd14ed9d4236
John01Alves <johnalvessr@protonmail.com>
2021-03-27 14:58:44 -0300

    adicionando a raquete.

--- ping_pong.py
+++ ping_pong.py
@@ -3,11 +3,25 @@ import pygame
 # Cores.
 preto = 0, 0, 0
 branco = 255, 255, 255
+verde = 0, 255, 0
 
 # Configurando a tela.
 tamanho = 800, 600
 tela = pygame.display.set_mode(tamanho)
+tela_retangulo = tela.get_rect()
 
+
+class Raquete:
+    def __int__(self, tamanho):
+        self.imagem = pygame.Surface(tamanho)
+        self.imagem.fill(verde)
+        self.imagem_retangulo = self.imagem.get_rect()
+
+    def realiza(self):
+        tela.blit(self.imagem, self.imagem_retangulo)
+
+
+raquete = Raquete((10, 50))
 fim = False
 
 # Iniciando a tela.
@@ -16,4 +30,5 @@ while not fim:
         if evento.type == pygame.QUIT:  # Condição de saida.
             fim = True
     tela.fill(preto)
+    raquete.realiza()
     pygame.display.update()
